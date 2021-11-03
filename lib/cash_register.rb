abd095695f7f3cc9fbdde0ce30fd23bb23b92880
Oluwayemisi Soremekun <oluwayemisi.soremekun@gmail.com>
2021-11-03 16:34:12 -0700

    commit tree 25bf2f69b59cfaa374dcc64057d98eef91c9200a

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,40 @@
+require "pry"
 
+class CashRegister
+
+    attr_accessor :total, :discount, :title, :items, :last_price
+
+ 
+
+    def initialize(discount=0)
+        @total = 0
+        @discount = discount
+        @items = []
+    end
+
+    def add_item(title, price, qty=1)
+        qty.times { @items << title }
+                
+        
+        @last_price = price*qty
+        self.total += price*qty
+    end
+
+    def apply_discount
+        if (@discount > 0)
+            self.total -= (self.discount/100.0)*self.total
+            "After the discount, the total comes to $#{self.total.to_i}."
+        else
+            "There is no discount to apply."
+        end
+    end
+
+    def items 
+        @items 
+    end
+
+    def void_last_transaction
+        @total -= @last_price
+    end
+
+end
